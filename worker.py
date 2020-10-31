64c1004ae194c033db40a3cf9e0a880d7bd46e6d
hahnarahyun <hahnarahyun@gmail.com>
2020-10-30 17:44:01 -0700

    adding logs

--- worker.py
+++ worker.py
@@ -1,7 +1,6 @@
 import boto3
 import time
 def fibonacci(n):
-    print("loop: ", n)
     if n <= 0:
         print("incorect input")
     elif n == 1:
@@ -17,9 +16,12 @@ sqs = boto3.resource('sqs', region_name='us-west-1')
 queue = sqs.get_queue_by_name(QueueName='Sustainability-GD-Queue')
 
 if __name__ == "__main__":
+    count = 0
     while True:
+        count = 1 + count
+        print("while loop: ", count)
         for message in queue.receive_messages(MessageAttributeNames=['Fibonacci'], MaxNumberOfMessage=1):
-            print('Hello')
+            print("processing message on sqs")
             number_text='0'
             if message.message_attributes is not None:
                 number = (message.message_attributes.get('Fibonacci').get('StringValue'))
@@ -28,5 +30,6 @@ if __name__ == "__main__":
                 number = int(number)
             print(fibonacci(number))
             print('{0}{1}'.format(message.body, number_text))
+            print('deleting message')
             message.delete()
         time.sleep(5)
